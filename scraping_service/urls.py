ce7889cb51d7753fd89e8e9ccd21ab224d77f3fd
Roman B <intomatar@gmail.com>
2020-07-23 02:43:57 +0600

    Initialize project

---
+++ scraping_service/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from scraping.views import home_view, list_view
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('list/', list_view, name='list'),
+    path('', home_view, name='home'),
+]

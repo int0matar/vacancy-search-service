a01f65d31bcb7367abcda325d972269abcc2ecb5
Roman B <intomatar@gmail.com>
2020-07-28 14:55:30 +0600

    feat: add authenticate form

--- scraping_service/urls.py
+++ scraping_service/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from scraping.views import home_view, list_view
 
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('list/', list_view, name='list'),
-    path('', home_view, name='home'),
+    path('accounts/', include(('accounts.urls', 'accounts'))),
 ]

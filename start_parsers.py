3c91ef2f0c0cbc5c80b2fd2ce17589548104c0a0
int0matar <intomatar@gmail.com>
2020-09-09 23:54:11 +0600

    refactor: update parser script

--- start_parsers.py
+++ start_parsers.py
@@ -1,22 +1,22 @@
 import os
 import sys
 import django
-import codecs
 import asyncio
 from django.db import DatabaseError
 from django.contrib.auth import get_user_model
-from scraping.models import City, Language, Vacancy, UrlAddress, ErrorLog
-from scraping.parsers import *
-
 
 project_path = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(project_path)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'scraping_service.settings'
 django.setup()
 
+from scraping.models import Vacancy, Url, Error
+from scraping.parsers import *
+
 User = get_user_model()
 
-jobs_data, errors_data = [], []
+data_for_recording_vacancies = []
+data_for_recording_errors = []
 
 parsers = (
     (work_ua, 'work_ua'),
@@ -24,56 +24,77 @@ parsers = (
     (dou_ua, 'dou_ua'),
     (djinni_co, 'djinni_co'),
 )
-def get_unique_pair():
-    """Returns unique id pairs of tuple by city and language"""
-    qset = User.objects.filter(email_subscription=True).values()
-    unique_pairs = set((q['city_id'], q['language_id']) for q in qset)
-    return unique_pairs
 
-def get_url(id_pairs):
-    """Returns list with dictionary by unique tuple of get_unique_pair method
-    and field UrlAddress.url_data"""
-    qset = UrlAddress.objects.all().values()
-    url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qset}
+
+def get_unique_pair():
+    """
+    Returns unique id pairs of tuple by city and language
+    """
+    subscribed_users = User.objects.filter(is_subscriber=True).values()
+    unique_pair_of_user = set(
+        (value_id['location_id'], value_id['specialty_id'])
+        for value_id in subscribed_users
+    )
+    return unique_pair_of_user
+
+
+def get_url(pair_id):
+    """
+    Returns list with dictionary by unique tuple of get_unique_pair method
+    and field UrlAddress.url_data
+    """
+    url_values = Url.objects.all().values()
+    url_dict = {
+        (value['location_id'], value['specialty_id']): value['url_json_field']
+        for value in url_values
+    }
     urls = []
-    for pair in id_pairs:
-        temp = {}
-        temp['city'] = pair[0]
-        temp['language'] = pair[1]
-        temp['url_data'] = url_dict[pair]
-        urls.append(temp)
+    for pair in pair_id:
+        temp = {'location': pair[0], 'specialty': pair[1]}
+        url_data = url_dict.get(pair)
+        if url_data:
+            temp['url_json_field'] = url_dict.get(pair)
+            urls.append(temp)
     return urls
+
 
 unique_pairs = get_unique_pair()
 url_list = get_url(unique_pairs)
 
 
-async def main(value):
-"""Asynchronous execution of parser functions"""
-    function, url, city, language = value
-    job_log, error_log = await loop.run_in_executor(None, function, url,
-                                                    city, language)
-    jobs_data.extend(job_log)
-    errors_data.extend(error_log)
+async def execute(task_pool):
+    """
+    Asynchronous execution of parser functions
+    """
+    function, location, specialty, url = task_pool
+    vacancy_log, error_log = await loop.run_in_executor(None,
+                                                        function,
+                                                        location,
+                                                        specialty,
+                                                        url)
+    data_for_recording_vacancies.extend(vacancy_log)
+    data_for_recording_errors.extend(error_log)
 
 loop = asyncio.get_event_loop()
-temp_tasks = [
-    (function, data['url_data'][parser], data['city'], data['language'])
-    for data in url_list
-    for function, parser in parsers
+
+instruction = [
+    (function,
+     data['url_json_field'][parser],
+     data['location_fk'],
+     data['specialty_fk'])
+    for data in url_list for function, parser in parsers
 ]
-tasks = asyncio.wait([loop.create_task(main(temp)) for temp in temp_tasks])
+tasks = asyncio.wait([loop.create_task(execute(task)) for task in instruction])
 loop.run_until_complete(tasks)
 loop.close()
 
-
-for record in jobs_data:
-    record_vacancy = Vacancy(**record)
+for record in data_for_recording_vacancies:
+    to_record = Vacancy(**record)
     try:
-        record_vacancy.save()
+        to_record.save()
     except DatabaseError:
         pass
 
-if errors_data:
-    error_dump = ErrorLog(error_data=error_log)
+if data_for_recording_errors:
+    error_dump = Error(error_json_field=data_for_recording_errors)
     error_dump.save()

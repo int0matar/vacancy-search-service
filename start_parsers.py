51280a128201ff1f698deb4f0521400b5cc78b70
int0matar <intomatar@gmail.com>
2020-09-26 19:26:34 +0600

    refactor: update naming

--- start_parsers.py
+++ start_parsers.py
@@ -13,18 +13,16 @@ django.setup()
 from scraping.models import Vacancy, Url, Error
 from scraping.parsers import *
 
+
 User = get_user_model()
 
 data_for_recording_vacancies = []
 data_for_recording_errors = []
 
-parsers = (
-    (work_ua, 'work_ua'),
-    (rabota_ua, 'rabota_ua'),
-    (dou_ua, 'dou_ua'),
-    (djinni_co, 'djinni_co'),
-)
-
+parsers = ((work_ua, 'work_ua'),
+           (rabota_ua, 'rabota_ua'),
+           (dou_ua, 'dou_ua'),
+           (djinni_co, 'djinni_co'))
 
 def get_unique_pair():
     """
@@ -32,41 +30,41 @@ def get_unique_pair():
     """
     subscribed_users = User.objects.filter(is_subscriber=True).values()
     unique_pair_of_user = set(
-        (value_id['location_id'], value_id['specialty_id'])
+        (value_id['location_fk_id'], value_id['specialty_fk_id'])
         for value_id in subscribed_users
     )
     return unique_pair_of_user
 
 
-def get_url(pair_id):
+def get_pair_id_with_url(pair_id):
     """
     Returns list with dictionary by unique tuple of get_unique_pair method
-    and field UrlAddress.url_data
+    and field Url.url_json_field
     """
     url_values = Url.objects.all().values()
-    url_dict = {
-        (value['location_id'], value['specialty_id']): value['url_json_field']
-        for value in url_values
+    pair_id_with_urls = {
+        (value_id['location_fk_id'], value_id['specialty_fk_id']):
+        value_id['url_json_field'] for value_id in url_values
     }
     urls = []
-    for pair in pair_id:
-        temp = {'location': pair[0], 'specialty': pair[1]}
-        url_data = url_dict.get(pair)
-        if url_data:
-            temp['url_json_field'] = url_dict.get(pair)
-            urls.append(temp)
+    for id_ in pair_id:
+        set_pair = {'location_fk_id': id_[0], 'specialty_fk_id': id_[1]}
+        get_url = pair_id_with_urls.get(id_)
+        if get_url:
+            set_pair['url_json_field'] = pair_id_with_urls.get(id_)
+            urls.append(set_pair)
     return urls
 
 
-unique_pairs = get_unique_pair()
-url_list = get_url(unique_pairs)
+unique_pair = get_unique_pair()
+url_list = get_pair_id_with_url(unique_pair)
 
 
-async def execute(task_pool):
+async def execute(task):
     """
     Asynchronous execution of parser functions
     """
-    function, location, specialty, url = task_pool
+    function, location, specialty, url = task
     vacancy_log, error_log = await loop.run_in_executor(None,
                                                         function,
                                                         location,
@@ -80,8 +78,8 @@ loop = asyncio.get_event_loop()
 instruction = [
     (function,
      data['url_json_field'][parser],
-     data['location_fk'],
-     data['specialty_fk'])
+     data['location_fk_id'],
+     data['specialty_fk_id'])
     for data in url_list for function, parser in parsers
 ]
 tasks = asyncio.wait([loop.create_task(execute(task)) for task in instruction])

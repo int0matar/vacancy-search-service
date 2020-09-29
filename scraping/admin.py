2909201c5dede5f23c3e7edb55f5c16d9ebbfd84
int0matar <intomatar@gmail.com>
2020-09-29 06:41:29 +0600

    refactor: update the names

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 
-from .models import City, Language, Vacancy, UrlAddress, ErrorLog
+from .models import Location, Specialty, Vacancy, Url, Error
 
 
-@admin.register(City)
-class CityAdmin(admin.ModelAdmin):
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
     search_fields = ('id', 'name', 'slug')
     list_display = ('id', 'name', 'slug')
     list_display_links = ('name',)
 
 
-@admin.register(Language)
-class LanguageAdmin(admin.ModelAdmin):
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
     search_fields = ('id', 'name', 'slug')
     list_display = ('id', 'name', 'slug')
     list_display_links = ('name',)
@@ -19,21 +19,22 @@ class LanguageAdmin(admin.ModelAdmin):
 
 @admin.register(Vacancy)
 class VacancyAdmin(admin.ModelAdmin):
-    search_fields = ('id', 'title', 'company', 'city', 'language',
-                     'description', 'timestamp')
-    list_filter = ('language', 'company', 'timestamp')
-    list_display = ('id', 'title', 'company', 'city', 'language', 'timestamp')
+    search_fields = ('id', 'location', 'specialty', 'title', 'company',
+                     'description', 'date',)
+    list_display = ('id', 'location', 'specialty', 'title', 'company', 'date',)
+    list_filter = ('specialty', 'company', 'date')
     list_display_links = ('title',)
 
 
-@admin.register(ErrorLog)
-class ErrorLogAdmin(admin.ModelAdmin):
-    search_fields = ('id', 'error_data', 'timestamp')
-    list_filter = ('timestamp',)
-    list_display = ('id', 'timestamp', 'error_data')
+@admin.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'location', 'specialty', 'url_json')
+    list_display = ('id', 'location', 'specialty', 'url_json')
 
 
-@admin.register(UrlAddress)
-class UrlAddressAdmin(admin.ModelAdmin):
-    search_fields = ('id', 'city', 'language', 'url_data')
-    list_display = ('id', 'city', 'language', 'url_data')
+@admin.register(Error)
+class ErrorAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'error_json', 'date')
+    list_display = ('id', 'date', 'error_json')
+    list_filter = ('date',)
+    list_display_links = ('error_json',)

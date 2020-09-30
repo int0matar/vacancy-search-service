0ecfa9a982372695592ec4908cf3d1462b570ef1
int0matar <intomatar@gmail.com>
2020-09-30 20:52:04 +0600

    refactor: update the pagination

--- scraping/paginations.py
+++ scraping/paginations.py
@@ -1,32 +1,35 @@
 from django.core.paginator import Paginator
 
 
-def get_page_object(object, request_page_number, number_of_page):
-    paginator = Paginator(object, number_of_page)
+def get_page_object(obj, request_page_number, number_of_page):
+    paginator = Paginator(obj, number_of_page)
     page_number = request_page_number
     page_object = paginator.get_page(page_number)
     return page_object
 
-def get_is_paginated(object):
-    is_paginated = object.has_other_pages()
+
+def get_paginate(pagination_obj):
+    is_paginated = pagination_obj.has_other_pages()
     return is_paginated
 
-def get_has_next(object, city, language):
-    if object.has_next():
-        next_url = '?city={}&language={}&page={}'.format(
-            city,
-            language,
-            object.next_page_number())
+
+def get_next_page(pagination_obj, location, specialty):
+    if pagination_obj.has_next():
+        next_url = '?location={}&specialty={}&page={}'.format(
+            location,
+            specialty,
+            pagination_obj.next_page_number())
     else:
         next_url = ''
     return next_url
 
-def get_has_previous(object, city, language):
-    if object.has_previous():
-        prev_url = '?city={}&language={}&page={}'.format(
-            city,
-            language,
-            object.previous_page_number())
+
+def get_previous_page(pagination_obj, location, specialty):
+    if pagination_obj.has_previous():
+        prev_url = '?location={}&specialty={}&page={}'.format(
+            location,
+            specialty,
+            pagination_obj.previous_page_number())
     else:
         prev_url = ''
     return prev_url
